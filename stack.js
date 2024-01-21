826719368febfab1ec05599b4c5f6cbd4a516ea3
fatuma18 <fatuma18@cs.washington.edu>
2024-01-20 19:08:57 -0800

    stack

---
+++ stack.js
@@ -0,0 +1,14 @@
+function calPoints(operations){
+ let recode = []
+ for(let i = 0; i < operations; i++){
+     operations[i] = recode
+     recode ++
+ }
+ return recode
+};
+let result = new calPoints()
+let score = [3,4]
+
+
+
+console.log(calPoints(score))
